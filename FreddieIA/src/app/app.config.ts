9807f348d1fe7c6c3632f2aefaad1e68468e5a0e
keeevvinn05 <barrosok526@gmail.com>
2025-06-30 10:04:54 +0200

    initialize new peojwct

---
+++ FreddieIA/src/app/app.config.ts
@@ -0,0 +1,26 @@
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { routes } from './app.routes';
+import { SimpleMarkdownPipe } from './shared/pipes/markdown.pipe';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
+        importProvidersFrom(
+      SimpleMarkdownPipe
+    )
+  ]
+};
